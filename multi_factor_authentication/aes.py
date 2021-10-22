c235a781ac7e437ce695f23d36edace8296051a0
Hiong Kai Han <kaihan00000@gmail.com>
2021-10-22 17:01:21 +0800

    Minor changes

--- multi_factor_authentication/aes.py
+++ multi_factor_authentication/aes.py
@@ -6,9 +6,13 @@ import os
 # A random key
 #key = binascii.unhexlify('000102030405060708090A0B0C0D0E0F')
 #text = binascii.unhexlify('00112233445566778899AABBCCDDEEFF')
-
+s1 = 1234567890123456
+s2 = 1111222233334444
+s3 = s1 ^ s2
+text = (b's3')
 key = (b"1111222233334444")
-text = (b"1234567890123456")
+iv = (b"1111222233334444")
+text = binascii.unhexlify('000302050704050a0a03020107000102')
 text2 = (b"5w4lj9nek0dpz1o73assgsx4pg6pj73ztjr8wz5bkzk3qtcj5miexhqajka7re4c")
 
 encryptor = AES.new(key, AES.MODE_ECB)
--- multi_factor_authentication/cbc.py
+++ multi_factor_authentication/cbc.py
@@ -4,17 +4,23 @@ import os
 
 # A random key
 #key = binascii.unhexlify('000102030405060708090A0B0C0D0E0F')
-iv = binascii.unhexlify('000102030405060708090A0B0C0D0E0F')
-key = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f'
+#iv = binascii.unhexlify('000102030405060708090A0B0C0D0E0F')
+#key = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f'
 
-print(key)
+key = (b"1111222233334444")
+iv = (b"1111222233334444")
+text = (b"1234567890123456")
+
+#print(key)
 
 encryptor = AES.new(key, AES.MODE_CBC, iv)
 decryptor = AES.new(key, AES.MODE_CBC, iv)
 
-text = binascii.unhexlify('00112233445566778899AABBCCDDEEFF')
+#text = binascii.unhexlify('00112233445566778899AABBCCDDEEFF')
 
 ciphertext = encryptor.encrypt(text)
+print(ciphertext)
 print(binascii.hexlify(ciphertext).upper())
 plaintext = decryptor.decrypt(ciphertext)
+print(plaintext)
 print(binascii.hexlify(plaintext).upper())
